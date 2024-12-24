968423fa033e88a8ead2127f05bcd01e6e43abf3
Rose Liu <rose@byanyothername.me>
2024-12-24 17:02:02 -0500

    Process CSV

---
+++ src/_data/processed.mjs
@@ -0,0 +1,30 @@
+import csv from 'csvtojson';
+
+const rawCsvPath = 'src/_data/clean-2024.csv';
+
+export default async function () {
+    // convert csv to json
+
+    var processed = await csv({
+        noheader: false,
+        headers: [
+            'id',
+            'author',
+            'title',
+            'booksPledged',
+            'originalLink',
+            'asin',
+            'cleanLink',
+            'wide',
+            'spfbo',
+            'spfboCohort',
+            'direct',
+            'directLink',
+            'intl'
+        ],
+        checkType: true
+    }).fromFile(rawCsvPath);
+    console.log(processed);
+
+    return processed;
+}
